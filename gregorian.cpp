ab7547d9761d4d32a1ef3159e46d73f6b6a8da58
Joel Pettersson <petterssonjoel@gmail.com>
2009-10-11 21:59:42 +0200

    Fixade och donade lite i Gregorian ...
    ... som förberedelse inför Julian och som repetition av vad ecksun
    skrivit. :)

--- gregorian.cpp
+++ gregorian.cpp
@@ -3,12 +3,6 @@ namespace lab2 {
 
     Gregorian::Gregorian(int y, int m, int d) : DateCommon(y,m,d) {
     }
-    // Gregorian::Gregorian(int year, int month, int day) : t_year(year), t_month(month), t_day(day) {
-    // Gregorian::Gregorian(int year, int month, int day) {
-        // this->t_year = year;
-        // this->t_month = month;
-        // this->t_day = day;
-    // }
 
     /** 
      * Increases the month with n
@@ -58,25 +52,32 @@ namespace lab2 {
     }
 
     /**
-     * http://en.wikipedia.org/wiki/Julian_day#Converting_between_Gregorian_calendar_date_and_Julian_Day_Number
+     * Returns the Julian day number, which is the interval of time in days
+     * since January 1, 4713 BC Greenwich noon.
+     *
+     * This method is valid for all Gregorian calendar dates after November 23,
+     * which indeed covers all years from 1858 to 2558.
+     *
+     * http://en.wikipedia.org/wiki/Julian_day
+     * #Converting_between_Gregorian_calendar_date_and_Julian_Day_Number
      */
-    int Gregorian::mod_julian_day() const { // vad är detta?
+    int Gregorian::mod_julian_day() const { 
         return (1461 * (t_year + 4800 + (t_month - 14)/12))/4 +(367 * (t_month - 2 - 12 * ((t_month - 14)/12)))/12 - (3 * ((t_year + 4900 + (t_month - 14)/12)/100))/4 + t_day - 32075;
     }
 
 
-    /**
+    /** 
      * Returns true if the current year is a leap year
      * 
-     * "    Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100; the centurial years that are exactly divisible by 400 are still leap years. For example, the year 1900 is not a leap year; the year 2000 is a leap year. "
+     * Every year that is exactly divisible by four is a leap year, except for
+     * years that are exactly divisible by 100; the centurial years that are
+     * exactly divisible by 400 are still leap years. For example, the year
+     * 1900 is not a leap year; the year 2000 is a leap year.
      */
-    bool Gregorian::leap_year() const {
-        if (t_year % 400)
-            return true;
-        if (t_year % 100)
-            return false;
-        if (t_year % 4)
-            return true;
+    bool Gregorian::leap_year() const { 
+        if (t_year % 400) return true; 
+        if (t_year % 100) return false; 
+        if (t_year % 4) return true; 
         return false;
     }
 
