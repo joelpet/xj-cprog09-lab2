fcceade5be4a32f42fd5e5eeed4858aaa6567ee3
Joel Pettersson <petterssonjoel@gmail.com>
2009-10-12 20:58:13 +0200

    Gjorde om custom_test
    så att vi använder datum där gX == jX. Dock tycks jämförelsen faila på
    g1==j1, vilket antagligen är helt korrekt. Återstår alltså att se över
    detta.

--- custom_test.cpp
+++ custom_test.cpp
@@ -16,71 +16,92 @@ int main() {
 
     // set up some test objects 
 
-    Gregorian a(1998, 9, 13);
-    Gregorian b(1998, 9, 13);
-    Gregorian c(2000, 10, 5);
-    Gregorian d(2000, 9, 13);
-    Gregorian e(1998, 9, 1);
-    Gregorian f(1998, 8, 31);
-    Gregorian g(1998, 8, 1);
-    Gregorian h(1999, 11, 1); 
-    Gregorian ng;
+    Gregorian g1(1900, 1, 1);
+    Gregorian g2(1900, 1, 13);
+    Gregorian g3(1858, 11, 16);
+    Gregorian g4(1858, 11, 17);
+    Gregorian g5(1858, 11, 18);
+    Gregorian gnow;
 
-    Julian aj(1998, 9, 13);
-    Julian bj(1998, 9, 13);
-    Julian cj(2000, 10, 5);
-    Julian dj(2000, 9, 13);
-    Julian ej(1998, 9, 1);
-    Julian fj(1998, 8, 31);
-    Julian gj(1998, 8, 1);
-    Julian nj;
+    Julian j1(1899, 12, 20);
+    Julian j2(1900, 1, 1);
+    Julian j3(1858, 11, 4);
+    Julian j4(1858, 11, 5);
+    Julian j5(1858, 11, 6);
+    Julian jnow;
 
-    // Test compare operators
-    assert(a == b);
-    assert(a != c);
-    assert(a != d);
-    assert(a != e);
-    assert(c - a > 0);
-    assert(a - c < 0);
-    assert(e - d < 0); 
-    assert(a < c);
-    assert(c > a);
-    assert(a <= b);
-    assert(b >= a);
+    // operator==
+    assert(g1 == g1);
+    assert(g4 == g4);
+    assert(j1 == j4);
+    assert(j4 == j4);
+    assert(g1 == j1);
+    assert(g2 == j2);
+    assert(g3 == j3);
+    assert(g4 == j4);
+    assert(g5 == j5);
+    assert(gnow == jnow);
+
+    // operator!=
+    assert(g1 != g2);
+    assert(j1 != j2);
+    assert(j3 != g4);
+    assert(j4 != j5);
+
+    // operator<
+    assert(g3 < j4);
+    assert(g4 < j5);
+    assert(j3 < g4);
+    assert(j4 < g5);
+
+    // operator>
+    assert(g4 > j3);
+    assert(g5 > j4);
+    assert(j4 > g3);
+    assert(j5 > g4);
+
+    // operator-
+    assert(g5 - j3 == 2);
+    assert(j3 - g5 == -2);
+    assert(j4 - g4 == 0);
+
+    // operator=
+    
+    // operator++
+    
+    // operator--
+
+    // operator+=
+
+    // operator-=
+
+    // year()
+    assert(g1.year() == 1900);
+    assert(g2.year() == 1900);
+    assert(g3.year() == 1858);
+    assert(g4.year() == 1858);
+    assert(g5.year() == 1858);
+    assert(gnow.year() == 2009);
+
+    assert(j1.year() == 1899);
+    assert(j2.year() == 1900);
+    assert(j3.year() == 1858);
+    assert(j4.year() == 1858);
+    assert(j5.year() == 1858);
+    assert(jnow.year() == 2009);
 
 
-    g.add_month(1);
-    assert(g == e);
 
-    // std::cout << g << std::endl;
-    g.add_month(15);
-    assert(g == h);
-    std::cout << g << std::endl;
-    std::cout << "|" << std::endl;
-    g.add_month(-16);
-    std::cout << "|" << std::endl;
-    std::cout << g << std::endl;
-    std::cout << "|" << std::endl;
-    std::cout << e << std::endl;
-    assert(g == e);
 
-    --e;
-    // std::cout << e << std::endl;
-    assert(e == f);
 
-    --e;
-    ++e;
-    assert(e == f);
+    // mod_julian_day
 
-    e = Gregorian(1998,9,1);
+    // add_day
+    
+    // add_month
 
-    e += 12;
-    assert(e == a);
-    c -= 22;
-    assert(c == d); // Osäker på att jag räknat rätt, dubbelkolla om det blir fel här
+    // add_year
 
-    std::cout << a << std::endl;
-    std::cout << "|" << std::endl;
 
     std::cout << "end custom tests" << std::endl;
 
