5e38934271a51c4aa9db8b42b69d2eafc0976417
Linus Wallgren <ecksun@gmail.com>
2009-10-14 00:50:25 +0200

    la till testfall från peket

--- custom_calendar_test.cpp
+++ custom_calendar_test.cpp
@@ -10,6 +10,33 @@ int main() {
 
     std::cout << "Starting custom tests" << std::endl;
 
+    std::cout << "----------------------------------------" << std::endl;
+    Calendar<Gregorian> cal;
+    cal.set_date(2000, 12, 2);
+    cal.add_event("Basketträning", 4, 12, 2000);
+    cal.add_event("Basketträning", 11, 12, 2000);
+    cal.add_event("Nyårsfrukost", 1, 1, 2001);
+    cal.add_event("Första advent", 1); // år = 2000, månad = 12
+    cal.add_event("Vårdagjämning", 20, 3); // år = 2000
+    cal.add_event("Julafton", 24, 12);
+    cal.add_event("Kalle Anka hälsar god jul", 24); // också på julafton
+    cal.add_event("Julafton", 24);  // En likadan händelse samma datum ska
+                                    // ignoreras och inte sättas in i kalendern
+    cal.add_event("Min första cykel", 20, 12, 2000);
+    cal.remove_event("Basketträning", 4);
+    std::cout << cal;   // OBS! Vårdagjämning och första advent är
+                        // före nuvarande datum och skrivs inte ut
+    std::cout << "----------------------------------------" << std::endl;
+    cal.remove_event("Vårdagjämning", 20, 3, 2000);
+    cal.remove_event("Kalle Anka hälsar god jul", 24, 12, 2000);
+    cal.set_date(2000, 11, 2);
+    if (! cal.remove_event("Julafton", 24)) {
+        std::cout << " cal.remove_event(\"Julafton\", 24) tar inte"<< std::endl
+            << " bort något eftersom aktuell månad är november" << std::endl;
+    }
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << cal;
+
     // set up some test objects 
     std::cout << "Setting up test objects ";
 
